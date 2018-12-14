52897e1508873d988268368cdc55302666a8c182
lara olivar <45249662+laraolivar@users.noreply.github.com>
2018-12-14 13:49:57 +0800

    butt

---
+++ button/button/CommandDemoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace button
+{
+    internal class CommandDemoViewModel : INotifyPropertyChanged
+    {
+        double number = 1;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public CommandDemoViewModel()
+        {
+            MultiplyBy2Command = new Command(() => Number *= 2);
+            DivideBy2Command = new Command(() => Number /= 2);
+        }
+
+        public double Number
+        {
+            set
+            {
+                if (number != value)
+                {
+                    number = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Number"));
+                }
+            }
+            get
+            {
+                return number;
+            }
+        }
+        public Command MultiplyBy2Command { private set; get; }
+        public Command DivideBy2Command { private set; get; }
+        }
+
+    }
+}
